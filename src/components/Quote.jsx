cc4bb5e78abce3e0de6c6685ed25f04abfe262f5
Ezequiel Ramirez <ezequielram@gmail.com>
2021-09-07 11:34:25 -0300

    desarrollo de app y funcionalidad de api

---
+++ src/components/Quote.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Quote = ({quote}) => {
+    return (
+
+        <p>
+            {quote.text} <br />
+            <span>- {quote.author}</span>
+        </p>
+
+    );
+};
+
+export default Quote
